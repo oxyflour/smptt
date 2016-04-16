a4f5f4a9ae6b7bf5e03a443ce3963505d6eec3d6
oxyflour <oxyflour@hotmail.com>
2016-04-16 13:34:44 +0800

    key, cert and ca are all required to enable tls

--- cli.js
+++ cli.js
@@ -9,20 +9,18 @@ var net = require('net'),
 
 program
 	.version(package.version)
-	.option('-p, --port <n>', 'port number, required',
-		(r, p) => p.concat(parseInt(r)), [ ])
-	.option('--key <file>', 'tls key file, required when using tls')
-	.option('--cert <file>', 'tls crt file, optional when using tls')
-	.option('--ca <file>', 'ca crt file, optional when using tls')
-	.option('-P, --peer [addr]', 'peer address like localhost:8081, required as sender',
-		(r, p) => p.concat(r), [ ])
+	.option('-p, --port <number>', 'port number, required', (r, p) => p.concat(parseInt(r)), [ ])
+	.option('-P, --peer [addr]', 'peer address like localhost:8081, required as sender', (r, p) => p.concat(r), [ ])
 	.option('-t, --target <addr>', 'target address like localhost:8082, required as receiver')
+	.option('--key <file>', 'ssl key file, required when using tls')
+	.option('--cert <file>', 'ssl crt file, required when using tls')
+	.option('--ca <file>', 'ca crt file, required when using tls')
 	.parse(process.argv)
 
 var tlsOpts = {
-	withTLS: !!program.key,
-	cert: program.cert && fs.readFileSync(program.cert),
+	withTLS: program.key && program.cert && program.ca,
 	key: program.key && fs.readFileSync(program.key),
+	cert: program.cert && fs.readFileSync(program.cert),
 	ca: program.ca && fs.readFileSync(program.ca),
 }
 
@@ -33,17 +31,24 @@ function parseAddr(addr, ext) {
 		port: parseInt(st[1] || 8080),
 
 		withTLS: tlsOpts.withTLS,
-		cert: tlsOpts.cert,
 		key: tlsOpts.key,
+		cert: tlsOpts.cert,
 		ca: tlsOpts.ca,
 
 		requestCert: true,
-		// TODO: make it work
-		rejectUnauthorized: false,
+		rejectUnauthorized: true,
 	}
 }
 
-if (program.port.length && program.peer.length && !program.target) {
+if (program.port.length === 0) {
+	console.log('at least one port number is required')
+	process.exit(-1)
+}
+else if (tlsOpts.withTLS && (!tlsOpts.key || !tlsOpts.cert || !tlsOpts.ca)) {
+	console.log('key, cert and ca files are required when using tls')
+	process.exit(-1)
+}
+else if (program.port.length && program.peer.length && !program.target) {
 	console.log('starting as sender at port ' + program.port.join(', '))
 
 	var handler = new smptt.Sender(program.peer.map(parseAddr))
