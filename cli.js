94c3b6b49debd48a3c08e1a500b1a2fbfeca7667
oxyflour <oxyflour@hotmail.com>
2016-06-05 14:12:23 +0800

    allow setting listen address

--- cli.js
+++ cli.js
@@ -9,7 +9,7 @@ var net = require('net'),
 
 program
 	.version(package.version)
-	.option('-p, --port <number>', 'listen port, required', (r, p) => p.concat(parseInt(r)), [ ])
+	.option('-p, --port <addr>', 'listen address or port, required', (r, p) => p.concat(r), [ ])
 	.option('-P, --peer <addr>', 'peer address or port, required as sender', (r, p) => p.concat(r), [ ])
 	.option('-t, --target <addr>', 'target address, required as receiver')
 	.option('--pfx <file>', 'pfx file containning ca/crt/key, required to use tls')
@@ -22,29 +22,31 @@ var tlsOpts = {
 	rejectUnauthorized: true,
 }
 
-function parseAddr(addr) {
+function parseAddr(addr, host) {
 	var st = parseInt(addr) === +addr ?
-		['', addr] :
-		addr.split(':')
+		[host, addr] : addr.split(':')
 	return {
 		host: st[0] || 'localhost',
 		port: parseInt(st[1] || 8080),
 	}
 }
 
+var addrLocalhost = addr => parseAddr(addr, 'localhost'),
+	addrAny = addr => parseAddr(addr, '0.0.0.0')
+
 if (program.port.length && program.peer.length && !program.target) {
 	console.log('starting as sender at port ' + program.port.join(', '))
 
 	var useTLS = addr => tlsOpts.withTLS ? Object.assign(addr, tlsOpts) : addr,
-		handler = new smptt.Sender(program.peer.map(parseAddr).map(useTLS))
-	program.port.forEach(port => net.createServer(handler).listen(port))
+		handler = new smptt.Sender(program.peer.map(addrLocalhost).map(useTLS))
+	program.port.map(addrAny).forEach(addr => net.createServer(handler).listen(addr))
 }
 else if (program.port.length && program.target && !program.peer.length) {
 	console.log('starting as receiver at port ' + program.port.join(', '))
 
-	var handler = new smptt.Receiver(parseAddr(program.target)),
+	var handler = new smptt.Receiver(addrLocalhost(program.target)),
 		server = _ => tlsOpts.withTLS ? tls.createServer(tlsOpts, handler) : net.createServer(handler)
-	program.port.forEach(port => server().listen(port))
+	program.port.map(addrAny).forEach(addr => server().listen(addr))
 }
 else {
 	program.outputHelp()
