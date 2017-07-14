09adba1cd6b1a79b51cb85bf102b57e7968d4fb4
oxyflour <oxyflour@hotmail.com>
2017-07-15 07:18:14 +0800

    add index.js

--- cli.js
+++ cli.js
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
 'use strict'
 
-const net = require('net'),
-  fs = require('fs'),
-  http = require('http'),
+const fs = require('fs'),
   program = require('commander'),
   packageJson = require('./package.json'),
-  createPool = require('./pool'),
-  protocol = require('./protocol')
+  startServer = require('./')
 
 program
   .version(packageJson.version)
@@ -32,186 +29,9 @@ if (!program.listen.length && !(program.peer.length && program.forward.length)) 
 }
 
 if (!fs.existsSync(program.pfx)) {
+  console.error('pfx file is required!')
   program.outputHelp()
   process.exit(-1)
 }
 
-const pfx = fs.readFileSync(program.pfx)
-function parse(addr) {
-  const st = addr.split(':'),
-    port = +st.pop(),
-    host = st.pop(),
-    servername = st.pop() || host
-  return {
-    pfx, port, host, servername,
-    requestCert: true,
-    rejectUnauthorized: true,
-  }
-}
-
-const clientPeers = [ ]
-if (program.peer.length && program.forward.length) {
-  const pool = createPool(program)
-  program.peer.forEach(addr => {
-    protocol.connect(parse(addr), peer => {
-      peer.startupTime = Date.now()
-      peer.lastPings = [ ]
-      peer.averagePing = program.pingMax * 1000
-
-      peer.urlRemote = addr
-      console.log('[C] connected to ' + addr)
-      clientPeers.indexOf(peer) === -1 && clientPeers.push(peer)
-      pool.eachConn((conn, id) => conn.add(peer).send('open', id, 0, forwarding[id]))
-
-      peer.on('error', err => {
-        console.log('[C] error from ' + addr + ': ', err)
-        clientPeers.indexOf(peer) >= 0 && clientPeers.splice(clientPeers.indexOf(peer), 1)
-        pool.eachConn(conn => conn.remove(peer))
-      })
-      peer.on('disconnect', _ => {
-        console.log('[C] disconnected from ' + addr)
-        clientPeers.indexOf(peer) >= 0 && clientPeers.splice(clientPeers.indexOf(peer), 1)
-        pool.eachConn(conn => conn.remove(peer))
-      })
-
-      peer.recv('ping', tick => {
-        peer.send('pong', tick)
-      })
-      peer.recv('pong', tick => {
-        peer.lastPings = peer.lastPings.concat(Date.now() % 0xffffffff - tick).slice(-5)
-        peer.averagePing = peer.lastPings.reduce((a, b) => a + b, 0) / peer.lastPings.length
-      })
-      peer.recv('data', (id, index, body) => {
-        pool.has(id) && pool.open(id).recv(index, body, peer)
-      })
-      peer.recv('req', (id, index) => {
-        pool.has(id) && pool.open(id).rescue(index)
-      })
-      peer.recv('ack', (id, index) => {
-        pool.has(id) && pool.open(id).acknowledge(index)
-      })
-    })
-  })
-
-  const forwarding = { }
-  program.forward.forEach(forward => {
-    const st = forward.split(':'),
-      port = st.pop(), host = st.pop(),
-      addr = host + ':' + port
-
-    const server = net.createServer(sock => {
-      const id = Math.floor(Math.random() * 0xffffffff),
-        conn = pool.open(id, sock)
-      forwarding[id] = addr
-      clientPeers.forEach(peer => conn.add(peer).send('open', id, 0, forwarding[id]))
-    })
-
-    console.log('[C] forwarding ' + forward)
-    st.length > 1 ? server.listen(+st[1], st[0]) : server.listen(+st[0])
-  })
-
-  setInterval(_ => {
-    const peer = clientPeers[Math.floor(Math.random() * clientPeers.length)]
-    if (peer) {
-      peer.averagePing = program.pingMax * 1000
-      peer.send('ping', Date.now() % 0xffffffff)
-    }
-    const now = Date.now()
-    clientPeers.forEach(peer => {
-      now - peer.lastActive > program.pingMax * 1000 && peer.destroy()
-    })
-  }, program.pingInterval * 1000)
-}
-
-const serverPeers = [ ]
-if (program.listen.length) {
-  const pool = createPool(program)
-  program.listen.forEach(addr => {
-    protocol.listen(parse(addr), peer => {
-      peer.startupTime = Date.now()
-      peer.lastPings = [ ]
-      peer.averagePing = program.pingMax * 1000
-
-      const addr = peer.addrRemote
-      console.log('[S] peer connected', addr)
-      serverPeers.indexOf(peer) === -1 && serverPeers.push(peer)
-
-      peer.on('error', err => {
-        console.log('[S] error from ' + addr + ': ', err)
-        serverPeers.indexOf(peer) >= 0 && serverPeers.splice(serverPeers.indexOf(peer), 1)
-        pool.eachConn(conn => conn.remove(peer))
-      })
-      peer.on('disconnect', _ => {
-        console.log('[S] peer disconnected from ', addr)
-        serverPeers.indexOf(peer) >= 0 && serverPeers.splice(serverPeers.indexOf(peer), 1)
-        pool.eachConn(conn => conn.remove(peer))
-      })
-
-      peer.recv('ping', tick => {
-        peer.send('pong', tick)
-      })
-      peer.recv('pong', tick => {
-        peer.lastPings = peer.lastPings.concat(Date.now() % 0xffffffff - tick).slice(-5)
-        peer.averagePing = peer.lastPings.reduce((a, b) => a + b, 0) / peer.lastPings.length
-      })
-      peer.recv('open', (id, index, addr) => {
-        pool.open(id, addr.toString()).add(peer)
-      })
-      peer.recv('data', (id, index, body) => {
-        pool.has(id) && pool.open(id).recv(index, body, peer)
-      })
-      peer.recv('req', (id, index) => {
-        pool.has(id) && pool.open(id).rescue(index)
-      })
-      peer.recv('ack', (id, index) => {
-        pool.has(id) && pool.open(id).acknowledge(index)
-      })
-    })
-
-    console.log('[S] listening at ' + addr)
-  })
-
-  setInterval(_ => {
-    const peer = serverPeers[Math.floor(Math.random() * serverPeers.length)]
-    if (peer) {
-      peer.averagePing = program.pingMax * 1000
-      peer.send('ping', Date.now() % 0xffffffff)
-    }
-    const now = Date.now()
-    serverPeers.forEach(peer => {
-      now - peer.lastActive > program.pingMax * 1000 && peer.destroy()
-    })
-  }, program.pingInterval * 1000)
-}
-
-if (program.apiAddress) {
-  const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-      res.setHeader('Content-Type', 'text/html')
-      fs.createReadStream(__dirname + '/monitor.html').pipe(res)
-    }
-    else if (req.url === '/status.json') {
-      res.setHeader('Access-Control-Allow-Origin', '*')
-      res.setHeader('Access-Control-Request-Method', '*')
-      res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
-      res.setHeader('Access-Control-Allow-Headers', req.header ? req.header.origin : '*')
-      const peerStat = peer => ({
-        startup: peer.startupTime,
-        addr: peer.urlRemote || peer.addrRemote,
-        ping: peer.averagePing,
-        sent: peer.bytesSent,
-        recv: peer.bytesRecv,
-      })
-      res.end(JSON.stringify({
-        pingMax: program.pingMax * 1000,
-        version: packageJson.version,
-        server: serverPeers.map(peerStat),
-        client: clientPeers.map(peerStat),
-      }, null, 2))
-    }
-  })
-  const st = program.apiAddress.split(':')
-  st.length > 1 ? server.listen(st[1], st[0]) : server.listen(st[0])
-
-  console.log('[S] api server at ' + program.apiAddress)
-}
+startServer(program)
---
+++ index.js
@@ -0,0 +1,181 @@
+'use strict'
+
+const net = require('net'),
+  fs = require('fs'),
+  http = require('http'),
+  packageJson = require('./package.json'),
+  createPool = require('./pool'),
+  protocol = require('./protocol')
+
+module.exports = function(options) {
+  const pfx = fs.readFileSync(options.pfx)
+  function parseAddr(addr) {
+    const st = addr.split(':'),
+      port = +st.pop(),
+      host = st.pop(),
+      servername = st.pop() || host
+    return {
+      pfx, port, host, servername,
+      requestCert: true,
+      rejectUnauthorized: true,
+    }
+  }
+
+  const clientPeers = [ ]
+  if (options.peer.length && options.forward.length) {
+    const pool = createPool(options)
+    options.peer.forEach(addr => {
+      protocol.connect(parseAddr(addr), peer => {
+        peer.startupTime = Date.now()
+        peer.lastPings = [ ]
+        peer.averagePing = options.pingMax * 1000
+
+        peer.urlRemote = addr
+        console.log('[C] connected to ' + addr)
+        clientPeers.indexOf(peer) === -1 && clientPeers.push(peer)
+        pool.eachConn((conn, id) => conn.add(peer).send('open', id, 0, forwarding[id]))
+
+        peer.on('error', err => {
+          console.log('[C] error from ' + addr + ': ', err)
+          clientPeers.indexOf(peer) >= 0 && clientPeers.splice(clientPeers.indexOf(peer), 1)
+          pool.eachConn(conn => conn.remove(peer))
+        })
+        peer.on('disconnect', _ => {
+          console.log('[C] disconnected from ' + addr)
+          clientPeers.indexOf(peer) >= 0 && clientPeers.splice(clientPeers.indexOf(peer), 1)
+          pool.eachConn(conn => conn.remove(peer))
+        })
+
+        peer.recv('ping', tick => {
+          peer.send('pong', tick)
+        })
+        peer.recv('pong', tick => {
+          peer.lastPings = peer.lastPings.concat(Date.now() % 0xffffffff - tick).slice(-5)
+          peer.averagePing = peer.lastPings.reduce((a, b) => a + b, 0) / peer.lastPings.length
+        })
+        peer.recv('data', (id, index, body) => {
+          pool.has(id) && pool.open(id).recv(index, body, peer)
+        })
+        peer.recv('req', (id, index) => {
+          pool.has(id) && pool.open(id).rescue(index)
+        })
+        peer.recv('ack', (id, index) => {
+          pool.has(id) && pool.open(id).acknowledge(index)
+        })
+      })
+    })
+
+    const forwarding = { }
+    options.forward.forEach(forward => {
+      const st = forward.split(':'),
+        port = st.pop(), host = st.pop(),
+        addr = host + ':' + port
+
+      const server = net.createServer(sock => {
+        const id = Math.floor(Math.random() * 0xffffffff),
+          conn = pool.open(id, sock)
+        forwarding[id] = addr
+        clientPeers.forEach(peer => conn.add(peer).send('open', id, 0, forwarding[id]))
+      })
+
+      console.log('[C] forwarding ' + forward)
+      st.length > 1 ? server.listen(+st[1], st[0]) : server.listen(+st[0])
+    })
+  }
+
+  const serverPeers = [ ]
+  if (options.listen.length) {
+    const pool = createPool(options)
+    options.listen.forEach(addr => {
+      protocol.listen(parseAddr(addr), peer => {
+        peer.startupTime = Date.now()
+        peer.lastPings = [ ]
+        peer.averagePing = options.pingMax * 1000
+
+        const addr = peer.addrRemote
+        console.log('[S] peer connected', addr)
+        serverPeers.indexOf(peer) === -1 && serverPeers.push(peer)
+
+        peer.on('error', err => {
+          console.log('[S] error from ' + addr + ': ', err)
+          serverPeers.indexOf(peer) >= 0 && serverPeers.splice(serverPeers.indexOf(peer), 1)
+          pool.eachConn(conn => conn.remove(peer))
+        })
+        peer.on('disconnect', _ => {
+          console.log('[S] peer disconnected from ', addr)
+          serverPeers.indexOf(peer) >= 0 && serverPeers.splice(serverPeers.indexOf(peer), 1)
+          pool.eachConn(conn => conn.remove(peer))
+        })
+
+        peer.recv('ping', tick => {
+          peer.send('pong', tick)
+        })
+        peer.recv('pong', tick => {
+          peer.lastPings = peer.lastPings.concat(Date.now() % 0xffffffff - tick).slice(-5)
+          peer.averagePing = peer.lastPings.reduce((a, b) => a + b, 0) / peer.lastPings.length
+        })
+        peer.recv('open', (id, index, addr) => {
+          pool.open(id, addr.toString()).add(peer)
+        })
+        peer.recv('data', (id, index, body) => {
+          pool.has(id) && pool.open(id).recv(index, body, peer)
+        })
+        peer.recv('req', (id, index) => {
+          pool.has(id) && pool.open(id).rescue(index)
+        })
+        peer.recv('ack', (id, index) => {
+          pool.has(id) && pool.open(id).acknowledge(index)
+        })
+      })
+
+      console.log('[S] listening at ' + addr)
+    })
+  }
+
+  setInterval(_ => {
+    const peers = serverPeers.concat(clientPeers)
+
+    const peer = peers[Math.floor(Math.random() * peers.length)]
+    if (peer) {
+      peer.averagePing = options.pingMax * 1000
+      peer.send('ping', Date.now() % 0xffffffff)
+    }
+
+    const now = Date.now()
+    peers.forEach(peer => {
+      now - peer.lastActive > options.pingMax * 1000 && peer.destroy()
+    })
+  }, options.pingInterval * 1000)
+
+  if (options.apiAddress) {
+    const server = http.createServer((req, res) => {
+      if (req.url === '/') {
+        res.setHeader('Content-Type', 'text/html')
+        fs.createReadStream(__dirname + '/monitor.html').pipe(res)
+      }
+      else if (req.url === '/status.json') {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Request-Method', '*')
+        res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET')
+        res.setHeader('Access-Control-Allow-Headers', req.header ? req.header.origin : '*')
+        const peerStat = peer => ({
+          startup: peer.startupTime,
+          addr: peer.urlRemote || peer.addrRemote,
+          ping: peer.averagePing,
+          sent: peer.bytesSent,
+          recv: peer.bytesRecv,
+        })
+        res.end(JSON.stringify({
+          pingMax: options.pingMax * 1000,
+          version: packageJson.version,
+          server: serverPeers.map(peerStat),
+          client: clientPeers.map(peerStat),
+        }, null, 2))
+      }
+    })
+    const st = options.apiAddress.split(':')
+    st.length > 1 ? server.listen(st[1], st[0]) : server.listen(st[0])
+
+    console.log('[S] api server at ' + options.apiAddress)
+  }
+}
